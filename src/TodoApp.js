78a1cfb159e94e4f79e8ecced2d922a3c77d8c0c
YogeshhhhhSahu <sahuyogesh150401@gmail.com>
2025-03-26 17:20:11 +0530

    Updated config.js with new URL

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";  // Import axios
 import "./TodoApp.css";
 
-const API_URL = "http://localhost:5000/api/tasks";  // Backend URL
+const API_URL = "hhttps://todo-backend-kxrz.onrender.com/api/todos";  // Backend URL
 
 function TodoApp() {
     const [tasks, setTasks] = useState([]);
